7d51f08c756d24bb92ff7a3be71a6e70a633d610
avimehenwal <avi.mehanwal@gmail.com>
2022-04-05 23:54:13 +0200

    feat: add product domain modal
    1. create new model
    2. register model in admin app

--- mobileBackend/orderapp/models.py
+++ mobileBackend/orderapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=500)
+    price = models.FloatField()
+    quantity_in_stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
