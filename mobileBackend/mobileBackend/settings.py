e6ef1e0ccbf7f7e0adf60577e2deb824891a9589
avimehenwal <avi.mehanwal@gmail.com>
2022-04-05 23:33:22 +0200

    feat: add django-rest-framework
    1. create seraalizers for models and queries
    2. use serializers in class based viewWSets
    3. update urlpatterns

--- mobileBackend/mobileBackend/settings.py
+++ mobileBackend/mobileBackend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom apps
+    'orderapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- mobileBackend/mobileBackend/urls.py
+++ mobileBackend/mobileBackend/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from orderapp import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('order/', include('orderapp.urls')),
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api/', include('orderapp.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
